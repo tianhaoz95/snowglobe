a2c0da74e19da472dad2e6282f203b5e7b4800c8
Tianhao Zhou <zhoutianhao03@meituan.com>
2026-02-15 14:55:54 -0800

    add test

---
+++ demo/integration_test/init_engine_test.dart
@@ -0,0 +1,22 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:integration_test/integration_test.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:snowglobedemo/src/rust/api/simple.dart';
+import 'package:snowglobedemo/src/rust/frb_generated.dart';
+
+void main() {
+  IntegrationTestWidgetsFlutterBinding.ensureInitialized();
+
+  testWidgets('initialize engine and exit', (WidgetTester tester) async {
+    try {
+      await RustLib.init();
+      final cacheDir = await getApplicationSupportDirectory();
+      await initEngine(cacheDir: cacheDir.path);
+      print('Engine initialized successfully.');
+    } catch (e, s) {
+      print('Error during engine initialization: $e');
+      print(s);
+      rethrow;
+    }
+  });
+}
